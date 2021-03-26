f7ba57ce16aab4f270bd76332fbdb4ae4be241aa
rafaelm71 <rafael00003@gmail.com>
2021-03-26 17:47:36 -0400

    Prims Adaptation

--- eShoppingXamarin/eShoppingXamarin.iOS/AppDelegate.cs
+++ eShoppingXamarin/eShoppingXamarin.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Prism;
+using Prism.Ioc;
 using UIKit;
 
 namespace eShoppingXamarin.iOS
@@ -23,9 +25,16 @@ namespace eShoppingXamarin.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new App(new IosInitializer()));
 
             return base.FinishedLaunching(app, options);
+        }
+        public class IosInitializer : IPlatformInitializer
+        {
+            public void RegisterTypes(IContainerRegistry containerRegistry)
+            {
+                
+            }
         }
     }
 }
